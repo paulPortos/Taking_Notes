acd141f872a9bcf0b46280030f69dd0f3accd96a
Auxilium <PaulVillacote.PGMR@gmail.com>
2024-07-28 10:23:24 +0800

    Var names changed

--- app/src/main/java/com/example/takingnotess/MainActivity.kt
+++ app/src/main/java/com/example/takingnotess/MainActivity.kt
@@ -6,49 +6,36 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-        class MainActivity : AppCompatActivity() {
-            @SuppressLint("MissingInflatedId")
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContentView(R.layout.activity_main)
-                val signup = findViewById<Button>(R.id.btnSignup)
-                val username = findViewById<EditText>(R.id.etusername)
-                val password = findViewById<EditText>(R.id.etpassword)
-                val forgot = findViewById<Button>(R.id.btnforgot)
-                val loginnow = findViewById<Button>(R.id.btnloginnow)
-                val signupnow = findViewById<Button>(R.id.btnSignupnow)
 
-
-                // LOGIN
-                loginnow.setOnClickListener {
-                    val usernamee = username.text.toString()
-                    val passwordd = password.text.toString()
-                    if (usernamee == "") {
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Please Enter your Username",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else if (passwordd == "") {
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Please Enter your password",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        Toast.makeText(
-                            this@MainActivity, "You Log in Successfully", Toast.LENGTH_SHORT
-                        ).show()
-
-                        val intent = Intent(this, HomeActivity::class.java)
-                        startActivity(intent)
-
-
-                    }
-                }
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnSignup: Button
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnForgot: Button
+    private lateinit var btnLoginNow: Button
+    private lateinit var btnSignupNow: Button
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnSignup = findViewById(R.id.btnSignup)
+        etUsername = findViewById(R.id.etusername)
+        etPassword = findViewById(R.id.etpassword)
+        btnForgot = findViewById(R.id.btnforgot)
+        btnLoginNow = findViewById(R.id.btnloginnow)
+        btnSignupNow = findViewById(R.id.btnSignupnow)
+        // LOGIN
+        btnLoginNow.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+            if (username == "") {
+                Toast.makeText(this@MainActivity, "Please Enter your Username", Toast.LENGTH_SHORT).show()
+            } else if (password == "") { Toast.makeText(this@MainActivity, "Please Enter your password", Toast.LENGTH_SHORT).show()
+            } else { Toast.makeText(this@MainActivity, "You Log in Successfully", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
             }
         }
+    }
+}
